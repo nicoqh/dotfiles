f83a8670c6a7c32f45dc330d725f461ae1548073
nicoqh <nicoqh@gmail.com>
2018-08-11 16:13:04 +0200

    change alias for launching nvim

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,7 +1,6 @@
 alias l='ls -lh --group-directories-first'
 alias ll='ls -lAh --group-directories-first'
 alias rm='rm -i'
-alias v='vim'
-alias nv='nvim'
+alias v='nvim'
 
 alias dotfiles="git --work-tree=$HOME/ --git-dir=$HOME/.local/share/dotfiles.git"
