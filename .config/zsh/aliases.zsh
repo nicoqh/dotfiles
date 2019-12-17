337cadbdfabd44cc998950817a4f3929c21b0463
nicoqh <nicoqh@gmail.com>
2019-12-17 21:24:25 +0100

    add new alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,6 +1,11 @@
 alias l='ls -lh --group-directories-first'
 alias ll='ls -lAh --group-directories-first'
 alias rm='rm -i'
+
 alias v='nvim'
+
+if type nvim > /dev/null 2>&1; then
+  alias vim='nvim'
+fi
 
 alias dotfiles="git --work-tree=$HOME/ --git-dir=$HOME/.local/share/dotfiles.git"
