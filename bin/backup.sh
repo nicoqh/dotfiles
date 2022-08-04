084809f5d0f3b19c9f6ff56e885486b92d185c00
nicoqh <nicoqh@gmail.com>
2022-08-04 13:05:24 +0200

    update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-SpiderOakONE --batchmode
+DATE=`date '+%Y-%m-%d %H:%M'`
+
+echo "$DATE: Starting ..." >> /home/nico/backup.log
+
+SpiderOakONE --batchmode >> /home/nico/backup.log
 
 export DISPLAY=:0
 /usr/bin/notify-send "Backup has run" --expire-time=30000 -i face-angel
